9bd9ae4b513fc99128d05057bfceb676379bd854
geraldkisilu <muligerald6@gmail.com>
2024-06-15 18:18:31 +0300

    commit

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,18 @@
 class Pet:
-    pass
 
-class Owner:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self,name,pet_type,owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+    @pet_type.setter
+    def pet_type(self,pet_type):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception("Invalid pet type")
+        self._pet_type = pet_type
