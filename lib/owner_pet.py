13f77b917ee9981dcddd673cf4372ca8ee026781
geraldkisilu <muligerald6@gmail.com>
2024-06-15 18:23:01 +0300

    commit

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -16,3 +16,19 @@ class Pet:
         if pet_type not in Pet.PET_TYPES:
             raise Exception("Invalid pet type")
         self._pet_type = pet_type
+class Owner:
+    def __init__ (self,name):
+        self.name = name
+        
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Input object is not of type Pet")
+        pet.owner = self            
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
